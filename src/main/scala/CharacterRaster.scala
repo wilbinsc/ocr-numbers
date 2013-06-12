c38bc631eaec7781355511e88d203555d072a179
Wilbin Chan <wilbin@eaiti.com>
2013-06-12 09:45:53 -0400

    various tweaks to knn

--- src/main/scala/CharacterRaster.scala
+++ src/main/scala/CharacterRaster.scala
@@ -9,7 +9,7 @@ class CharacterRaster(image: Array[Int]) {
 	val data = image
 	var pca: Array[Double] = null
 	var classification: Option[Char] = None
-	
+
 	def classify(c: Char) = {
 		this.classification = Some(c)
 		this
@@ -22,7 +22,7 @@ class CharacterRaster(image: Array[Int]) {
 	def draw() = {
 		val img = new BufferedImage(28, 28, BufferedImage.TYPE_BYTE_GRAY)
 		img.getRaster().setPixels(0, 0, 28, 28, data)
-		ImageIO.write(img, "png", new File(classification.get + ".png"))
+		ImageIO.write(img, "png", new File((if (classification != None) classification.get else "x") + ".png"))
 	}
 }
 
@@ -32,13 +32,24 @@ object CharacterRaster {
 		new CharacterRaster(data.split(",").map(_.toInt))
 	}
 
+	def fromCsvPcaData(data: String): CharacterRaster = {
+		val pca = data.split(",").map(_.toDouble)
+		val c = new CharacterRaster(null)
+		c.pca = pca
+		c
+	}
+
 	def fromCsvClassifiedImageData(data: String) = {
 		fromCsvImageData(data.substring(2)).classify(data.charAt(0))
 	}
 
-	def distance(c1: CharacterRaster, c2: CharacterRaster) = {
-		c1.data.zip(c2.data.toList).map((x: (Int, Int)) => ((x._1 - x._2) * (x._1 - x._2)).asInstanceOf[Long]).sum
+	def fromCsvClassifiedPcaData(data: String) = {
+		fromCsvPcaData(data.substring(2)).classify(data.charAt(0))
 	}
-	
+
+	def distance(c1: CharacterRaster, c2: CharacterRaster) = {
+		c1.data.zip(c2.data.toList).map((x: (Int, Int)) => ((x._1 - x._2) * (x._1 - x._2)).asInstanceOf[Double]).sum
+	}
+
 	def pcaDistance(c1: CharacterRaster, c2: CharacterRaster) = c1.pca.zip(c2.pca.toList).map((x: (Double, Double)) => ((x._1 - x._2) * (x._1 - x._2)).asInstanceOf[Double]).sum
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,7 @@ import scala.io.Source
 
 object Main {
 
+	val pcaN = List(40, 50, 60, 70, 80, 90, 100, 120, 140, 160)
 	def loadTraining() = {
 		for (line <- Source.fromFile("train.csv").getLines(); if (Character.isDigit(line.charAt(0))))
 			yield CharacterRaster.fromCsvClassifiedImageData(line)
@@ -16,16 +17,48 @@ object Main {
 			yield CharacterRaster.fromCsvImageData(line)
 	}
 
-	def knn(test: CharacterRaster, trainingSet: List[CharacterRaster], f: (CharacterRaster, CharacterRaster) => Double) = {
-		val neighbors = trainingSet.par.map((train) => (train.classification.get, f(test, train))).toList.sortBy(_._2)
-		val result = List(kfilter(neighbors, 3), kfilter(neighbors, 5), kfilter(neighbors, 10), kfilter(neighbors, 20))
-		// take the majority vote
-		val consensus = result.groupBy(identity).mapValues(_.size).toList.sortBy(-_._2).apply(0)._1
-		consensus :: result
+	def loadTrainingPca(n: Int) = {
+		for (line <- Source.fromFile("pca" + n).getLines())
+			yield CharacterRaster.fromCsvClassifiedPcaData(line)
 	}
 
-	def kfilter(neighbors: List[(Char, Double)], k: Int): Char = {
-		neighbors.slice(0, k).groupBy(_._1).mapValues(_.size).toList.sortBy(_._2 * -1).apply(0)._1
+	def loadTestPca(n: Int) = {
+		for (line <- Source.fromFile("testpca" + n).getLines())
+			yield CharacterRaster.fromCsvPcaData(line)
+	}
+
+	def knn(test: CharacterRaster, trainingSet: List[CharacterRaster], f: (CharacterRaster, CharacterRaster) => Double) = {
+		val neighbors = trainingSet.par.map((train) => (train.classification.get, f(test, train))).toList.sortBy(_._2)
+		List(
+			kfilter(neighbors, 5, true),
+			kfilter(neighbors, 5, false),
+			kfilter(neighbors, 7, true),
+			kfilter(neighbors, 7, false),
+			kfilter(neighbors, 10, true),
+			kfilter(neighbors, 10, false),
+			kfilter(neighbors, 15, true),
+			kfilter(neighbors, 15, false))
+	}
+
+	def knn(k: Int, test: CharacterRaster, trainingSet: List[CharacterRaster], f: (CharacterRaster, CharacterRaster) => Double) = {
+		val neighbors = trainingSet.par.map((train) => (train.classification.get, f(test, train))).toList.sortBy(_._2)
+		kfilter(neighbors, k, false)
+	}
+
+	def kfilter(neighbors: List[(Char, Double)], k: Int, uniformWeight: Boolean): Char = {
+		if (uniformWeight)
+			neighbors.slice(0, k)
+				.groupBy(_._1)
+				.mapValues(_.size)
+				.toList
+				.sortBy(_._2 * -1).apply(0)._1
+		else
+			neighbors.slice(0, k)
+				.map(pair => (pair._1, 1 / Math.sqrt(pair._2))) // map the label, distance pair to label 1/sqrt(distance) as an inverse weight
+				.groupBy(_._1)
+				.mapValues(_.map(_._2).sum)
+				.toList
+				.sortBy(_._2 * -1).apply(0)._1
 	}
 
 	def confidenceRating(count: Int, sampleSize: Int): Int = {
@@ -34,43 +67,66 @@ object Main {
 		else 1
 	}
 
-	def precomputePca() {
-		val trainingSet = loadTraining.toList
+	def buildPca(samples: List[CharacterRaster]) = {
 		val pca = new Pca
-		pca.setup(trainingSet.size, 28 * 28)
-		trainingSet.foreach((c) => pca.addSample(c.getDataAsDouble))
-		List(10, 20, 30, 40, 50, 70, 100).foreach(num => {
-			pca.computeBasis(num)
-			val file = new PrintWriter(new File("pca" + num))
-			trainingSet.foreach((c) => {
+		pca.setup(samples.size, 28 * 28)
+		samples.foreach((c) => pca.addSample(c.getDataAsDouble))
+		pca
+	}
+
+	def precomputePca(pca: Pca, n: Int, data: List[CharacterRaster], outfile: String) {
+		pca.computeBasis(n)
+		val file = new PrintWriter(new File(outfile))
+		data.foreach((c) => {
+			if (c.classification != None) {
 				file.write(c.classification.get)
 				file.write(',')
-				file.write(pca.transform(c.getDataAsDouble).mkString(","))
-				file.write('\n')
-			})
-			file.close()
+			}
+			file.write(pca.transform(c.getDataAsDouble).mkString(","))
+			file.write('\n')
 		})
+		file.close()
+	}
+
+	def crossValidate(): Unit = {
+		// laod the precomputed pca data
+		val data = loadTraining.toList
+		// split the training data into training and test sets, use 2% of training set for cross validation
+		val (testSet, trainingSet) = data.partition(_.hashCode() % 50 == 0)
+		// run the classification and output error rates
+		var error = new Array[Double](8)
+		for (
+			test <- testSet;
+			classification = knn(test, trainingSet, CharacterRaster.distance)
+		) {
+			for (i <- 0 until error.length; if classification(i) != test.classification.get)
+				error(i) = error(i) + 1
+		}
+		println(error.map(_ / testSet.size).toList)
+	}
+
+	def classify() = {
+		val trainingSet = loadTraining.toList
+		val testSet = loadTest.toList
+		val file = new PrintWriter(new File("weightedknn7.csv"))
+		for (test <- testSet) {
+			val classification = knn(7, test, trainingSet, CharacterRaster.distance)
+			file.print(classification)
+			file.print('\n')
+			file.flush
+		}
+		file.close()
 	}
 
 	def main(args: Array[String]): Unit = {
-		precomputePca()
-//		val trainingSet = loadTraining.toList
-//		val pca = new Pca
-//		pca.setup(trainingSet.size, 28 * 28)
-//		trainingSet.foreach((c) => pca.addSample(c.getDataAsDouble))
-//		pca.computeBasis(50)
-//		trainingSet.par.foreach((c) => c.pca = pca.transform(c.getDataAsDouble))
-//		println("PCA done")
-//		// val testSet = loadTest.toList
-//		// split the training set
-//		val split = trainingSet.partition(_.data.hashCode() % 50 == 0)
-//		// calculate the error rates for k = 1,3,5,10,20,50
-//		val errors: Array[Int] = new Array[Int](5)
-//		for (test <- split._1) {
-//			val classification = knn(test, split._2, CharacterRaster.pcaDistance)
-//			for (i <- 0 until classification.length; if classification(i) != test.classification.get)
-//				errors(i) = errors(i) + 1
-//		}
-//		errors.map(_.asInstanceOf[Double] / split._1.size).toArray.foreach(println)
+		//		val trainingSet = loadTraining.toList
+		//		val testSet = loadTest.toList
+		//		val pca = buildPca(trainingSet ++ testSet)
+		//		precomputePca(pca, 50, trainingSet, "pca50")
+		//		precomputePca(pca, 50, testSet, "testpca50")
+		classify()
+		// loadTest.toList.apply(12).draw()
+		// for (i <- 0 until 10)
+		//	crossValidate
 	}
 }
